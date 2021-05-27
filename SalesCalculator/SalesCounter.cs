dc199cf7f1ae7b2c0340a0e6157df69c10bfb832
飯野　結衣 <32089@ojs.ac.jp>
2021-05-27 15:50:51 +0900

    p70リスト2-22

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,43 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     //売上請求データ
     class SalesCounter {
-
         private List<Sale> _sales;
 
-        public SalesCounster(List<Sale> sales) {
-            _sales = sales;
+        //コンストラクタ
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        //List 2-15
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+            //List 2-17
+            //店舗売り上げを求める
+            public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<String, int> dict = new Dictionary<string, int>();
+            foreach(Sale sale in _sales) {
+                if(dict.ContainsKey(sale.ShopName))
+                    //既にコレクションへ店舗が登録されている
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    //コレクションへ店舗を登録
+                    dict[sale.ShopName] = sale.Amount; 
+            }
+            return dict;
         }
     }
 }
