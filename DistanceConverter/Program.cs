f7bf4a47f27eb71f6011f357952bf322a7c6157e
飯野　結衣 <32089@ojs.ac.jp>
2021-06-01 10:20:14 +0900

    p79第2章演習問題 2-2完了

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//メートルからフィート、フィートからメートルへ変換するプログラム
 namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
---
+++ Exercise2/FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    //フィートとメートルの単位変換クラス(静的クラス)
+    class FeetConverter {
+
+        private const double ratio = 0.3048;
+
+        //メートルからフィートを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
