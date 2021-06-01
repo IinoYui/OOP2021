a8a6d3c65238b904f9239cc2e896dfd0cd4bde39
飯野　結衣 <32089@ojs.ac.jp>
2021-06-01 09:32:07 +0900

    p79 第2章演習問題 問題2.1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            //2.1.3
+            var songs = new Song[] {
+                new Song("のび太くん０点","野比のび太",168),
+                new Song("おとなりのプリンセス","源静香",158),
+                new Song("スネ夫のおぼっちゃマンボ","骨川スネ夫",191),
+                new Song("ジャイアンにボエボエ","剛田武",168),
+                new Song("ひみつ道具数え歌","ドラえもん",250),
+            };
+            PrintSongs(songs);
+
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+
+            foreach(var song in songs) {
+                Console.WriteLine("{0},{1},{2}:{3}",song.Title, song.ArtistName, (song.Length/60),(song.Length%60));
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+
+    class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+        
+        //アーティスト名
+        public string ArtistName { get; set; }
+        
+        //演奏時間、単位は秒
+        public int Length { get; set; }
+
+        public Song(string Title,String ArtistName,int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+    }
+}
