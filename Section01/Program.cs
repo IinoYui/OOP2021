3695fc5adc60ea5e4cefae6a5fee2cfad3a3d39f
飯野　結衣 <32089@ojs.ac.jp>
2021-06-04 13:42:51 +0900

    P99第3章演習問題 コミット！！

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -7,27 +7,60 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var sc = new SampleCord();
 
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
 
-            //var count = sc.Count( numbers, delegate(int n) { return n % 2 == 0;} );
-            var count = sc.Count(numbers, n => n % 2 == 0 ) ;
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
 
-            Console.WriteLine(count);
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+
+
         }
 
-     }
-    
-    class SampleCord {
 
-        public int Count(int[] numbers, Predicate<int> judge) {
-            int count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true)
-                    count++;
-            }
-            return count;
+        //8か9で割り切れる数があるか
+        private static void Exercise1_1(List<int> numbers) {
+            var exist = numbers.Exists( n => n % 8 == 0 || n % 9 ==0 );
+
+            if (exist)
+                Console.WriteLine("存在しています。");
+            else
+                Console.WriteLine("存在していません。");
+        }
+
+
+        //各要素を2.0で割った値
+        private static void Exercise1_2(List<int> numbers) {
+            var exist = numbers.FindAll(n => n / 2 == 0);
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+        }
+
+
+        //値が50以上の要素を列挙する
+        private static void Exercise1_3(List<int> numbers) {
+            var query = numbers.Where(n => n >=  50);
+            foreach (int n in query)
+                Console.WriteLine(n);
+        }
+
+
+        //値を2倍にする
+        private static void Exercise1_4(List<int> numbers) {
+            var query = numbers.Select(n => n == n * 2);
+            foreach (var n in query)
+                Console.Write(n);
         }
     }
 }
