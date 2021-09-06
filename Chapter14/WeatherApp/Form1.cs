53ec065496204fcc710cf6c412e578e5f829a655
結衣 飯野 <32089@ojs.ac.jp>
2021-09-06 15:04:50 +0900

    0906授業プログラム

---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            switch (HyouziBox.Text)
+            {
+                case "前橋":
+                    num = 4210;
+                    break;
+                case "みなかみ":
+                    num = 4220;
+                    break;
+                case "宇都宮":
+                    num = 4110;
+                    break;
+                case "水戸":
+                    num = 4010;
+                    break;
+            }
+
+            var results = GetWeatherReportFromYahoo(num);
+            foreach (var s in results)
+            {
+                Tiiki.Text += s + "\r\n";
+            }
+        }
+
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode)
+        {
+            using (var wc = new WebClient())
+            {
+
+            }
+        }
+
+    }
+}
---
+++ RssReader/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace RssReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rss = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.lbTitles = new System.Windows.Forms.ListBox();
+            this.btRead = new System.Windows.Forms.Button();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // Rss
+            // 
+            this.Rss.AutoSize = true;
+            this.Rss.Font = new System.Drawing.Font("MV Boli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rss.Location = new System.Drawing.Point(29, 24);
+            this.Rss.Name = "Rss";
+            this.Rss.Size = new System.Drawing.Size(136, 31);
+            this.Rss.TabIndex = 0;
+            this.Rss.Text = "RssReader";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbUrl.Location = new System.Drawing.Point(179, 21);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(366, 31);
+            this.tbUrl.TabIndex = 1;
+            // 
+            // lbTitles
+            // 
+            this.lbTitles.FormattingEnabled = true;
+            this.lbTitles.ItemHeight = 12;
+            this.lbTitles.Location = new System.Drawing.Point(23, 86);
+            this.lbTitles.Name = "lbTitles";
+            this.lbTitles.Size = new System.Drawing.Size(132, 448);
+            this.lbTitles.TabIndex = 2;
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(565, 21);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(106, 31);
+            this.btRead.TabIndex = 3;
+            this.btRead.Text = "button1";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(179, 89);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(491, 447);
+            this.wbBrowser.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 571);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.lbTitles);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.Rss);
+            this.Name = "Form1";
+            this.Text = "32089";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Rss;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.ListBox lbTitles;
+        private System.Windows.Forms.Button btRead;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+    }
+}
+
